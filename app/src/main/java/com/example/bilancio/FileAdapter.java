6aa91fbd7741d8c607d9368d92d5e4075c858786
danielemaccagnan <63585453+th1italy@users.noreply.github.com>
2024-04-02 18:49:16 +0200

    condividi file

--- app/src/main/java/com/example/bilancio/FileAdapter.java
+++ app/src/main/java/com/example/bilancio/FileAdapter.java
@@ -1,17 +1,13 @@
 package com.example.bilancio;
 
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.core.content.FileProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.io.File;
@@ -20,11 +16,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class FileAdapter extends RecyclerView.Adapter<FileAdapter.FileViewHolder> {
-
     private ArrayList<File> mFiles;
     private Context context;
     private Set<Integer> selectedItems = new HashSet<>();
-    private boolean isSelectionMode = false;
 
     public FileAdapter(Context context, ArrayList<File> files) {
         this.context = context;
@@ -38,58 +32,34 @@ public class FileAdapter extends RecyclerView.Adapter<FileAdapter.FileViewHolder
         return new FileViewHolder(itemView);
     }
 
-
     @Override
     public void onBindViewHolder(@NonNull FileViewHolder holder, int position) {
         File currentFile = mFiles.get(position);
         holder.textViewFileName.setText(currentFile.getName());
 
+        holder.itemView.setBackgroundColor(selectedItems.contains(position) ? context.getResources().getColor(android.R.color.holo_blue_light) : context.getResources().getColor(android.R.color.transparent));
+
         holder.itemView.setOnClickListener(v -> {
-            if (isSelectionMode) {
-                toggleItemSelection(position, holder.itemView);
+            if (selectedItems.contains(position)) {
+                selectedItems.remove(position);
             } else {
-                openFile(currentFile);
+                selectedItems.add(position);
             }
+            notifyItemChanged(position);
         });
-
-        holder.itemView.setOnLongClickListener(v -> {
-            if (!isSelectionMode) {
-                isSelectionMode = true;
-                toggleItemSelection(position, holder.itemView);
-            } else {
-                clearSelection();
-            }
-            return true;
-        });
-
-    }
-
-
-    private void openFile(File file) {
-        Uri uri = FileProvider.getUriForFile(context, context.getApplicationContext().getPackageName() + ".provider", file);
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setDataAndType(uri, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
-        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        if (intent.resolveActivity(context.getPackageManager()) != null) {
-            context.startActivity(intent);
-        } else {
-            Toast.makeText(context, "Nessuna app trovata per aprire questo file", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    private void toggleItemSelection(int position, View itemView) {
-        if (selectedItems.contains(position)) {
-            selectedItems.remove(position);
-            itemView.setBackgroundColor(context.getResources().getColor(android.R.color.transparent));
-        } else {
-            selectedItems.add(position);
-            itemView.setBackgroundColor(context.getResources().getColor(android.R.color.holo_blue_light));
-        }
     }
 
     @Override
     public int getItemCount() {
         return mFiles.size();
+    }
+
+    public ArrayList<File> getSelectedFiles() {
+        ArrayList<File> selectedFiles = new ArrayList<>();
+        for (Integer position : selectedItems) {
+            selectedFiles.add(mFiles.get(position));
+        }
+        return selectedFiles;
     }
 
     public void removeSelectedItems() {
@@ -98,41 +68,27 @@ public class FileAdapter extends RecyclerView.Adapter<FileAdapter.FileViewHolder
             return;
         }
 
-        Set<File> filesToDelete = new HashSet<>();
+        ArrayList<File> toRemove = new ArrayList<>();
         for (Integer position : selectedItems) {
-            filesToDelete.add(mFiles.get(position));
-        }
-
-        for (File file : filesToDelete) {
+            File file = mFiles.get(position);
             if (file.delete()) {
-                mFiles.remove(file);
+                toRemove.add(file);
                 Toast.makeText(context, file.getName() + " eliminato.", Toast.LENGTH_SHORT).show();
             } else {
                 Toast.makeText(context, "Errore durante l'eliminazione di " + file.getName(), Toast.LENGTH_SHORT).show();
             }
         }
-        clearSelection();
-        notifyDataSetChanged(); // Aggiorna l'adapter per riflettere le modifiche.
-    }
-
-
-    private void clearSelection() {
+        mFiles.removeAll(toRemove);
         selectedItems.clear();
-        isSelectionMode = false;
         notifyDataSetChanged();
     }
 
     static class FileViewHolder extends RecyclerView.ViewHolder {
         TextView textViewFileName;
-        ImageView imageViewExcelIcon; // Riferimento esistente all'ImageView
 
         public FileViewHolder(View itemView) {
             super(itemView);
             textViewFileName = itemView.findViewById(R.id.textViewFileName);
-            imageViewExcelIcon = itemView.findViewById(R.id.imageViewExcelIcon);
-
         }
     }
-
-
 }
--- app/src/main/java/com/example/bilancio/filesalvati.java
+++ app/src/main/java/com/example/bilancio/filesalvati.java
@@ -1,12 +1,15 @@
 package com.example.bilancio;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -14,7 +17,6 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class filesalvati extends AppCompatActivity {
-
     private RecyclerView recyclerView;
     private FileAdapter adapter;
 
@@ -23,41 +25,35 @@ public class filesalvati extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.file_salvati);
 
-        // Setup del bottone per tornare alla MainActivity
         Button homeButton = findViewById(R.id.button_filesalvati);
-        Button deletebutton = findViewById(R.id.delete_button);
-        homeButton.setOnClickListener(view -> startActivity(new Intent(filesalvati.this, MainActivity.class)));
-        deletebutton.setOnClickListener(view -> {
-            if (adapter != null) {
-                adapter.removeSelectedItems();
-            }
+        Button deleteButton = findViewById(R.id.delete_button);
+        ImageView imageViewShareIcon = findViewById(R.id.imageViewShareIcon);
+
+        homeButton.setOnClickListener(view -> finish());
+        deleteButton.setOnClickListener(view -> {
+            adapter.removeSelectedItems();
+        });
+        imageViewShareIcon.setOnClickListener(v -> {
+            ArrayList<File> selectedFiles = adapter.getSelectedFiles();
+            shareFiles(selectedFiles);
         });
 
-        // Impostazione del RecyclerView
+
         recyclerView = findViewById(R.id.recyclerViewFiles);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        // Popolamento del RecyclerView con i file trovati
         populateRecyclerView();
     }
 
     private void populateRecyclerView() {
-        // Ottiene il riferimento alla directory dei documenti privati dell'app sotto "BILANCIO"
         File directory = new File(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), "BILANCIO");
-
-        // Verifica se la directory esiste
         if (!directory.exists()) {
-            // Se la directory non esiste, mostra un Toast e termina il metodo
             Toast.makeText(this, "La directory BILANCIO non esiste.", Toast.LENGTH_LONG).show();
             return;
         }
-
-        // Se la directory esiste, procedi con la ricerca dei file Excel
         ArrayList<File> fileList = findExcelFiles(directory);
         adapter = new FileAdapter(this, fileList);
         recyclerView.setAdapter(adapter);
     }
-
 
     private ArrayList<File> findExcelFiles(File directory) {
         ArrayList<File> fileList = new ArrayList<>();
@@ -65,16 +61,34 @@ public class filesalvati extends AppCompatActivity {
         if (files != null) {
             for (File file : files) {
                 if (file.isDirectory()) {
-                    // Ricerca ricorsiva in sottodirectory per trovare file Excel
                     fileList.addAll(findExcelFiles(file));
-                } else {
-                    // Aggiunge alla lista solo i file che terminano con .xls o .xlsx
-                    if (file.getName().endsWith(".xls") || file.getName().endsWith(".xlsx")) {
-                        fileList.add(file);
-                    }
+                } else if (file.getName().endsWith(".xls") || file.getName().endsWith(".xlsx")) {
+                    fileList.add(file);
                 }
             }
         }
         return fileList;
     }
+
+    private void shareFiles(ArrayList<File> files) {
+        if (files.isEmpty()) {
+            Toast.makeText(this, "Nessun file selezionato.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        ArrayList<Uri> uris = new ArrayList<>();
+        for (File file : files) {
+            Uri uri = FileProvider.getUriForFile(this, getApplicationContext().getPackageName() + ".provider", file);
+            uris.add(uri);
+        }
+
+        Intent shareIntent = new Intent(Intent.ACTION_SEND_MULTIPLE);
+        shareIntent.setType("application/*"); // Modifica se necessario in base al tipo di file
+        shareIntent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, uris);
+        shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        startActivity(Intent.createChooser(shareIntent, "Condividi File"));
+
+
+    }
+
 }
